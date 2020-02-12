566533c789bcee014ba559cb38f0883e0f230f65
Kelvin Reid <codesmithk@gmail.com>
2020-02-11 21:55:39 -0500

    Create SearchViewController.swift

---
+++ GitHubFollowers/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  GitHubFollowers
+//
+//  Created by Kelvin Reid on 2/11/20.
+//  Copyright © 2020 Kelvin Reid. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+
+    }
+  
+
+}
