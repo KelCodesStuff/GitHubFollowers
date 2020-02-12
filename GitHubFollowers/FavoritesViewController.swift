85a91c46a8feb003362f29284f35422558641c5b
Kelvin Reid <codesmithk@gmail.com>
2020-02-11 21:55:31 -0500

    Create FavoritesViewController.swift

---
+++ GitHubFollowers/FavoritesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FavoritesViewController.swift
+//  GitHubFollowers
+//
+//  Created by Kelvin Reid on 2/11/20.
+//  Copyright © 2020 Kelvin Reid. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+
+    }
+    
+
+
+
+}
