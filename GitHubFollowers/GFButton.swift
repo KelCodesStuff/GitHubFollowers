34899463f2d2b26b4b31d497cda14e0179fb6b91
Kelvin Reid <codesmithk@gmail.com>
2020-02-21 13:53:12 -0500

    Create GFButton.swift

---
+++ GitHubFollowers/GFButton.swift
@@ -0,0 +1,36 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Kelvin Reid on 2/12/20.
+//  Copyright © 2020 Kelvin Reid. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        setup()
+    }
+    
+    private func setup() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
