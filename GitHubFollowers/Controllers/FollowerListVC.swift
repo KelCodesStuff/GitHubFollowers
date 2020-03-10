6f3c514c770ca7a48a15b5f546872471bf5b03dc
Kelvin Reid <codesmithk@gmail.com>
2020-03-09 22:46:10 -0400

    Create FollowerListVC.swift

---
+++ GitHubFollowers/Controllers/FollowerListVC.swift
@@ -0,0 +1,52 @@
+//
+//  FollowerListVC.swift
+//  GitHubFollowers
+//
+//  Created by Kelvin Reid on 2/21/20.
+//  Copyright © 2020 Kelvin Reid. All rights reserved.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+    
+    //is set when data is passed from search vc
+    var username: String!
+    var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+        setupViewController()
+        getFollowers()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func setupViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func setupCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func getFollowers() {
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print(followers)
+                       
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
+}
