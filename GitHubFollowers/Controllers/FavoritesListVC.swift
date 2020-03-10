769cf3c7d18ef15683278e52405ab0233a151d6b
Kelvin Reid <codesmithk@gmail.com>
2020-03-09 22:45:25 -0400

    Create FavoritesListVC.swift

---
+++ GitHubFollowers/Controllers/FavoritesListVC.swift
@@ -0,0 +1,22 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Kelvin Reid on 2/11/20.
+//  Copyright © 2020 Kelvin Reid. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+
+    }
+
+    
+
+
+}
