7564a77741e79d6e04c32ee6407e77c7994d94e5
Kelvin Reid <codesmithk@gmail.com>
2020-03-09 22:45:51 -0400

    Create Follower.swift

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Kelvin Reid on 2/28/20.
+//  Copyright © 2020 Kelvin Reid. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
