c2e8ae0d1781dc077b88b4777a17661998db9902
Kelvin Reid <codesmithk@gmail.com>
2020-10-05 11:01:10 -0400

    Update FollowerListVC.swift

--- GitHubFollowers/Controllers/FollowerListVC.swift
+++ GitHubFollowers/Controllers/FollowerListVC.swift
@@ -32,10 +32,24 @@ class FollowerListVC: UIViewController {
     }
     
     func setupCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimiumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimiumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
     }
     
     func getFollowers() {
